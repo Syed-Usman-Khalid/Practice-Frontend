04de52e93fb340069bb9c5d3384399bdced76e08
Syed Usman Khalid <syedusmankhalid76@gmail.com>
2024-01-04 23:46:19 +0500

    Modify code

--- 2024 DSA/Arrays.cpp
+++ 2024 DSA/Arrays.cpp
@@ -2,16 +2,16 @@
 #include<vector>
 using namespace std;
 
-int findUnique(vector<int> arr)
-{
-    int ans = 0 ;
-    for(int i = 0; i <arr.size(); i++)
-    {
-        ans = ans ^ arr[i];
-    }
+// int findUnique(vector<int> arr)
+// {
+//     int ans = 0 ;
+//     for(int i = 0; i <arr.size(); i++)
+//     {
+//         ans = ans ^ arr[i];
+//     }
 
-    return ans;
-}
+//     return ans;
+// }
 
 int main()
 {
@@ -31,23 +31,44 @@ int main()
     // else 
     //     cout<<"key is not found\n";
 
-    int n;
-    cout<<"enter the size of element "<<endl;
-    cin>>n;
+    // int n;
+    // cout<<"enter the size of element "<<endl;
+    // cin>>n;
 
-    vector <int> arr(n);
-    cout<<"enter the element "<<endl;
-    // taking input
-    for(int i=0; i<arr.size(); i++)
+    // vector <int> arr(n);
+    // cout<<"enter the element "<<endl;
+    // // taking input
+    // for(int i=0; i<arr.size(); i++)
+    // {
+    //     cin>>arr[i];
+    // }
+
+    // int uniqueElement = findUnique(arr);
+
+    // cout<<"Unique element is "<<uniqueElement<<endl;
+    // cout<<endl<<"here a output of next problem "<<endl;
+
+    // -------------------------------------------------------------------
+
+    vector<int> arr = {1,2,3,4,6,8};
+    vector<int> brr = {3,4,10};
+    vector<int> ans;
+    
+    for(int i=0 ; i<arr.size(); i++)
     {
-        cin>>arr[i];
+        for(int j=0 ; j<brr.size(); j++)
+        {  
+            if(arr[i] == brr[j])
+            ans.push_back(arr[i]);
+            brr[j] = -1;
+        }
+        
     }
 
-    int uniqueElement = findUnique(arr);
-
-    cout<<"Unique element is "<<uniqueElement<<endl;
-
-
+    for(int i = 0; i<= ans.size(); i++)
+    {
+        cout<<"the intersection numbers are "<<ans[i]<<endl;
+    }
 
 
 
