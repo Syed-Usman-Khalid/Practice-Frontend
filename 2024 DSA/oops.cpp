7b03d33ea5403049bfdd71d6c30726725eea7681
Syed Usman Khalid <syedusmankhalid76@gmail.com>
2024-01-24 23:47:23 +0500

    commit done

--- 2024 DSA/oops.cpp
+++ 2024 DSA/oops.cpp
@@ -6,12 +6,19 @@ class Animal
     public:
     int gender;
 
+    Animal()
+    {
+        cout<<"you only named your pet \n";
+    }
 
 };
 
 int main()
 {
     cout<<"hello world\n";
+    Animal cat;
+
+    cout<<cat.gender<<endl;
 
     return 0;
 }
