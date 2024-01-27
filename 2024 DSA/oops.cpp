6195737a10cffddda5986ac0db685e1c25f0150c
Syed Usman Khalid <syedusmankhalid76@gmail.com>
2024-01-27 23:45:14 +0500

    OOPs

--- 2024 DSA/oops.cpp
+++ 2024 DSA/oops.cpp
@@ -11,16 +11,33 @@ class Animal
         cout<<"you only named your pet \n";
     }
 
+    void speak()
+    {
+        cout<<"speaking \n";
+    }
+
 };
 
+class Dog:public Animal{
+
+    public:
+
+    void speak()
+    {
+        cout<<"barking \n";
+    }
+
+};
 int main()
 {
-    cout<<"hello world\n";
+    // cout<<"hello world\n";
     Animal cat;
 
-    cout<<cat.gender<<endl;
+    // cout<<cat.gender<<endl;
+    Animal dog;
+    Dog bully;
+    bully.speak();
 
-    cout<<"suffering from fever so not work only push to continue the work";
-
+    
     return 0;
 }
