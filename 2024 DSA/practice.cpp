6b2d89a37fd4bd155365d5599fdca87dfc0c2911
Syed Usman Khalid <syedusmankhalid76@gmail.com>
2024-04-11 23:51:10 +0500

    2d

--- 2024 DSA/practice.cpp
+++ 2024 DSA/practice.cpp
@@ -5,11 +5,11 @@ using namespace std ;
 
 int main()
 {
-    int arr[7]={12,56,76,23,18};
+    // int arr[7]={12,56,76,23,18};
 
     // finding the length or size of an array.
     
-    int size = sizeof(arr)/sizeof(arr[0]);
+    // int size = sizeof(arr)/sizeof(arr[0]);
     
     // Fill whole arrays with same types of numbers. like whole array with 2,65 etc.
 
@@ -20,11 +20,11 @@ int main()
 
     // printing the whole array.
 
-    for (int i = 0 ; i< size ; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<"\n";
+    // for (int i = 0 ; i< size ; i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+    // cout<<"\n";
 
     // Linear Search 
 
@@ -38,26 +38,41 @@ int main()
 
     // Count O's and 1's in an array.
 
-    cout<<"yes";
-    cout<<"i am back";
-    cout<<"yes";
+    
+    //  Declaration of 2D array.
 
-    int arr[4][5];
-    for(int i = 0 ; i<4 ; i++)
+    int arr[2][2] = {2,3,5,4} ;
+    int key = 6;
+
+    // taking input in 2D array  
+
+    // cout<< "enter the 2d array "<<endl;
+
+    // for(int i = 0 ; i<2 ; i++)
+    //     {
+    //         for(int j = 0 ; j< 2 ;j++)
+    //         {
+    //             cin>>arr[i][j];
+    //         }
+    //     }
+    //     cout<< endl;
+
+    //  Printing the 2D array. 
+
+    for(int i = 0 ; i<2 ; i++)
         {
-            for(int j = 0 ; j< 5 ;j++)
+            for(int j = 0 ; j< 2 ;j++)
             {
-                cin>>arr[i][j];
+                // cout<<arr[i][j] << " ";
+                if (key == arr[i][j])
+                    cout<<"found\n";
+                else 
+                    cout<<"not found\n";
+
             }
+            cout<<endl;
         }
 
-    for(int i = 0 ; i<4 ; i++)
-        {
-            for(int j = 0 ; j< 5 ;j++)
-            {
-                cout<<arr[i][j];
-            }
-        }
 
     
     
