4f068ad1da622c99fb11b3ad60b712575966d95e
Syed Usman Khalid <syedusmankhalid76@gmail.com>
2024-05-07 07:48:34 +0500

    2d

--- 2024 DSA/2D-Arrays.cpp
+++ 2024 DSA/2D-Arrays.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int arr [2][3];
+    int arr [3][3];
     for(int i=0 ; i<3 ; i++)
     {
         for(int j = 0; j<3 ;j++)
@@ -20,6 +20,8 @@ int main()
         }
     cout<<endl;
     }
+    cout<<"yes";
+    
 
     
 
