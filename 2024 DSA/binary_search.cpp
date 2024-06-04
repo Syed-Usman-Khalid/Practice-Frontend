90e45dc6fa5b249ecd888a0aae35b549987434fb
Syed Usman Khalid <syedusmankhalid76@gmail.com>
2024-06-04 22:46:23 +0500

    yes

--- 2024 DSA/binary_search.cpp
+++ 2024 DSA/binary_search.cpp
@@ -4,20 +4,24 @@ using namespace std;
 
 int main()
 {
-    vector<int> nums = {2,4,6,8,10,12,13};
-    int s = 0, key = 11; 
-    int e = nums.size();
-    int mid = s + (e-s)/2;
+    // vector<int> nums = {2,4,6,8,10,12,13};
+    // int s = 0, key = 11; 
+    // int e = nums.size();
+    // int mid = s + (e-s)/2;
 
-    while(s<e)
-    {
-        if(nums[mid] == key)
-            cout<<"key is found\n";
-        else if(nums[mid] > key)
-            e = mid - 1;
-        else if(nums[mid] < key)
-            s= mid+1;
-    }
+    // while(s<e)
+    // {
+    //     if(nums[mid] == key)
+    //         cout<<"key is found\n";
+    //     else if(nums[mid] > key)
+    //         e = mid - 1;
+    //     else if(nums[mid] < key)
+    //         s= mid+1;
+    // }
+
+    cout<<"square root\n";
+
+
     
     return 0;
 
