21b87afbeb887ccf86b9267570a9454658ce2bf5
Syed Usman Khalid <syedusmankhalid76@gmail.com>
2024-08-23 23:10:31 +0500

    23-8

--- 2024 DSA/binary_search.cpp
+++ 2024 DSA/binary_search.cpp
@@ -57,7 +57,7 @@ int main()
     
     // ---------------------Book allocation problem -----------------------------
     
-    cout<<"ghar";
+    cout<<"after recovering from fever \n";
     
     
     
