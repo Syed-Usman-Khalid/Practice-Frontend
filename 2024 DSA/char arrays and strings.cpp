8d220b3404910383c2756da6d484cc8b48ea5d02
Syed Usman Khalid <syedusmankhalid76@gmail.com>
2024-09-23 23:56:12 +0500

    23-9

---
+++ 2024 DSA/char arrays and strings.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name[50];
+    // cout<<"Enter your name \n";
+    // cin.getline(name,50);
+
+    cin>>name[3];
+    cin>>name[49];
+
+    // cout<<name<<endl;
+
+    return 0 ;
+
+}
